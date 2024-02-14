c0a02d9bc3eebf6a5a345e3b8af66c4f9e9c87ba
giacomo <giacomo.sciolti@gmail.com>
2024-02-14 10:44:49 +0100

    TransactionIsRecent renaming

--- src/main/kotlin/com/gsciolti/transactionsapi/api/ApiConfiguration.kt
+++ src/main/kotlin/com/gsciolti/transactionsapi/api/ApiConfiguration.kt
@@ -3,7 +3,7 @@ package com.gsciolti.transactionsapi.api
 import com.gsciolti.transactionsapi.domain.statistics.GetAggregatedStatistics
 import com.gsciolti.transactionsapi.domain.transaction.CreateTransaction
 import com.gsciolti.transactionsapi.domain.transaction.DeleteAllTransactions
-import com.gsciolti.transactionsapi.domain.transaction.validate.TransactionIsInTheLast
+import com.gsciolti.transactionsapi.domain.transaction.validate.TransactionIsRecent
 import com.gsciolti.transactionsapi.repository.LatestTransactionsRepository
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
@@ -14,7 +14,7 @@ open class ApiConfiguration {
 
     private val timestampUpperBound: () -> Instant = { Instant.now() }
     private val timestampOffsetSeconds: Long = 60
-    private val validateTransaction = TransactionIsInTheLast(timestampOffsetSeconds, timestampUpperBound)
+    private val validateTransaction = TransactionIsRecent(timestampOffsetSeconds, timestampUpperBound)
     private val transactionsRepository = LatestTransactionsRepository(timestampOffsetSeconds, timestampUpperBound)
 
     @Bean
--- src/main/kotlin/com/gsciolti/transactionsapi/domain/transaction/validate/TransactionIsRecent.kt
+++ src/main/kotlin/com/gsciolti/transactionsapi/domain/transaction/validate/TransactionIsRecent.kt
@@ -5,10 +5,10 @@ import com.gsciolti.transactionsapi.domain.transaction.CreateTransaction
 import com.gsciolti.transactionsapi.domain.transaction.UnvalidatedTransaction
 import java.time.Instant
 
-class TransactionIsInTheLast(seconds: Long, end: () -> Instant) : ValidateTransaction {
+class TransactionIsRecent(lastSeconds: Long, end: () -> Instant) : ValidateTransaction {
 
     private val validate =
-        TransactionIsAfter { end().minusSeconds(seconds) }
+        TransactionIsAfter { end().minusSeconds(lastSeconds) }
             .and(TransactionIsBefore(end))
 
     override fun invoke(transaction: UnvalidatedTransaction): Either<CreateTransaction.Error, UnvalidatedTransaction> =
